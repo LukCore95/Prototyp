89519bdaa386919b93d9da80fe529c9a49fa705e
Luki <lukcore95@gmail.com>
2017-03-17 19:27:57 +0100

    SplashScreen (StartScreen) ANIMATE FINAL

--- app/src/main/java/zpi/prototyp/SplashScreen.java
+++ app/src/main/java/zpi/prototyp/SplashScreen.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 
 public class SplashScreen extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

20fb2932ab0f29aac0d684c4496a7707a9564b5c
Luki <lukcore95@gmail.com>
2017-03-19 01:50:02 +0100

    Dodanie stylizowanej mapy

--- app/src/main/java/zpi/prototyp/MainActivity.java
+++ app/src/main/java/zpi/prototyp/MainActivity.java
@@ -1,14 +1,45 @@
 package zpi.prototyp;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+            if (!success) {
+            }
+        } catch (Resources.NotFoundException e) {
+        }
+        LatLng wroclaw = new LatLng(51.1115591, 17.0597695);
+        CameraUpdate center = CameraUpdateFactory.newLatLng(wroclaw);
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+        googleMap.moveCamera(center);
+        googleMap.animateCamera(zoom);
+        googleMap.addMarker(new MarkerOptions().position(wroclaw).title("Marker in Wrocław"));
     }
 }

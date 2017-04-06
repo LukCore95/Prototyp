a3876c78b53d09513d16a5c79aa1a6c2dff99199
Luki <lukcore95@gmail.com>
2017-04-06 19:53:36 +0200

    Poprawiony czas splash screena

--- app/src/main/java/zpi/prototyp/SplashScreen.java
+++ app/src/main/java/zpi/prototyp/SplashScreen.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.widget.Toolbar;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class SplashScreen extends Activity {
 
-    //private static int SPLASH_TIME_OUT = 4500;
-    private static int SPLASH_TIME_OUT = 6500;
+    private static int SPLASH_TIME_OUT = 4500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
